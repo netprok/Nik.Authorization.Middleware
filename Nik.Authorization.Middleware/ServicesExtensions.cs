e020cbadf337254c87cb0c23d9c7a22bb794fa93
Ehsan Faramarzi <netpro@email.com>
2024-05-01 10:59:40 +0200

    names

--- Nik.Authorization.Middleware/ServicesExtensions.cs
+++ Nik.Authorization.Middleware/ServicesExtensions.cs
@@ -2,7 +2,7 @@
 
 public static class ServicesExtensions
 {
-    public static IServiceCollection AddNiCAuthorizationMiddleware(this IServiceCollection services)
+    public static IServiceCollection AddNikAuthorizationMiddleware(this IServiceCollection services)
     {
         services
             .AddAuthorization()
